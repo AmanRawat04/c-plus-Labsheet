81259cc4a85f0474736e3c12aadd3085d2efcbc7
Aman rawat <cu25310002@coeruniversity.ac.in>
2025-09-21 18:48:37 +0530

    C++ Labsheet-01

---
+++ Collage labsheet/lab16.cpp
@@ -0,0 +1,8 @@
+// Program show division by zero error
+#include <iostream>
+using namespace std;
+int main(){
+  int x=50,y=0;
+    cout<<"x/y:"<<x/y; // no answer flloating point exception
+    return 0;
+}
---
+++ Collage labsheet/lab17.cpp
@@ -0,0 +1,10 @@
+//program to uisng wrong operator by adding two numbers
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the two numbers:";
+    cin>>a>>b;
+    cout<<a*b<<endl; // answer : 49 error due to wrong operator
+    cout<<a+b<<endl; //answer: 14 expected answer
+}
