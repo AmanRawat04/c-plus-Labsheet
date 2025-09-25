dd3bd12657d5178c37a1ea9b2a23057f3aa2c514
AmanRawat04 <cu25310002@coeruniversity.ac.in>
2025-09-25 18:38:58 +0530

    // labsheet question 5

---
+++ Collage labsheet/Labsheet-04/lab5.cpp
@@ -0,0 +1,21 @@
+//Program to whether a year is leap or not
+#include <iostream>
+using namespace std;
+int main (){
+    int year;
+    cout<<"Enter the year : \n";
+    cin>>year;
+    if(year%400==0){
+        cout<<year<<"is a leap year.\n";
+    }
+    else if(year%100==0){
+cout<<year<<"is not a leap year.\n";
+    }
+    else if(year%4==0){
+        cout<<year<<"is a leap year.\n";
+    }
+    else{
+        cout<<year<<"is not a leap year.\n";
+    }
+    return 0;
+}
