1d419e60f93f43db4a7b5fb1bd10125bed476a55
AmanRawat04 <cu25310002@coeruniversity.ac.in>
2025-09-25 18:50:08 +0530

    // labsheet question 06

---
+++ Collage labsheet/Labsheet-04/lab6.cpp
@@ -0,0 +1,29 @@
+//Program to assign grades  based on marks(if else ladder)
+#include <iostream>
+using namespace std;
+int main(){
+    int marks;
+    char Grade;
+    cout<<"Enter the marks : \n";
+    cin>>marks;
+    if(marks>=90){
+        Grade = 'A';
+    }
+    else if(marks>=80){
+        Grade = 'B';
+    }
+    else if(marks>=70){
+        Grade = 'C';
+    }
+    else if(marks>=60){
+        Grade = 'D';
+    }
+    else if(marks>=50){
+        Grade = 'E';
+    }
+    else{
+        Grade ='F';
+    }
+    cout<<"Grade:"<<Grade<<endl;
+    return 0;
+}
